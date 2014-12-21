d6d14e117f48a06ad0bb98023dea46ddbb92991a
Nelson Crosby <nc@sourcecomb.com>
2014-12-22 10:48:37 +1300

    Created a skeleton

---
+++ src/main/groovy/io/github/nelsoncrosby/qsui/UIBuilder.groovy
@@ -0,0 +1,11 @@
+package io.github.nelsoncrosby.qsui
+
+/**
+ *
+ */
+class UIBuilder {
+    UIBuilder add(Component cmp) {
+
+        return this
+    }
+}
---
+++ src/main/groovy/io/github/nelsoncrosby/qsui/UIState.groovy
@@ -0,0 +1,19 @@
+package io.github.nelsoncrosby.qsui
+
+import org.newdawn.slick.GameContainer
+import org.newdawn.slick.Graphics
+
+/**
+ *
+ */
+class UIState {
+
+
+    void update(GameContainer gc, int delta) {
+
+    }
+
+    void render(GameContainer gc, Graphics gx) {
+
+    }
+}
